9226fe8b0d2a34e36eebf0d2da8c2533e2918128
unknown <shaunroy25@gmail.com>
2025-04-10 19:23:53 +0530

    initializing frontend & fixing tailwind

--- server/schemaTypes/index.js
+++ server/schemaTypes/index.js
@@ -1,4 +1,7 @@
+import comment from './comment'
 import user from './user'
+import save from './save'
+import postedBy from './postedBy'
+import pin from './pin'
 
-
-export const schemaTypes = [user]
+export const schemaTypes = [user, comment, save, postedBy, pin]
