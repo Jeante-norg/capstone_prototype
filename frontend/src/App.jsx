0b56ffc036bd6b66efea8fd4d017a89e4a4c19b9
Tej <jupitersorbeet@gmail.com>
2025-04-11 13:11:37 +0530

    tailwindcss working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-32 h-20 bg-amber-300 flex items-center justify-center">
+        <h1 className="text-sky-500 text-sm">Hello</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
